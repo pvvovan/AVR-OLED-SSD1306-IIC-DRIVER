70384810a6df7d11953d7044dc018c8c7e5876d9
PVVovan <pvvovan@gmail.com>
2022-06-20 22:51:26 +0200

    Show incremented value

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <util/delay.h>
+#include <stdio.h>
+
 #include "SSD1306.h"
 
 int main(void)
@@ -8,9 +11,17 @@ int main(void)
 
 	OLED_SetCursor(0, 0);		//set the cursor position to (0, 0)
 	OLED_Printf("Hi guys!");	//Print out some text
+	_delay_ms(3000);
 
+	double value = 0.0;
+	OLED_Clear();
 	while (true) {
-		;
+		value += 0.1;
+		char str[32];
+		snprintf(str, sizeof(str), "%5.2f", value);
+		OLED_SetCursor(0, 0);
+		OLED_Printf("value = %s%c", str, '%');
+		_delay_ms(1000);
 	}
 
 	return 0;	// never reached
